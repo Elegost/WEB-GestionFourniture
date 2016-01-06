474cb486200d836f66d5513b0912bd5261b10a90
Arcony <Galietti.jeanfranois@gmail.com>
2016-01-06 11:27:37 +0100

    Mise a jour graphique
    yavait deux graphique

--- GraphiqueAdmin.php
+++ GraphiqueAdmin.php
@@ -10,6 +10,13 @@
 </head>
 
 <body>
+ <div class="BlocHeader">
+		 <img id="logo" src="Image/logo.jpg" >
+		 <form action="Acceuil.php" method="post">
+			  <input id="ButtonSeDeconnecter" type="submit" value="Se déconnecter">
+		 </form>
+		<label id="IdUser" for="IdUser">Bonjour [MAIL] </label>
+	</div>
 <?php
     $servername = "localhost";
     $username = "root";
@@ -79,48 +86,8 @@ $result6cent=(($result6[0]*100)/($resultTotal[0]));
 	echo "</div>";
 
 	?>
-   <div class="BlocHeader">
-		 <img id="logo" src="Image/logo.jpg" >
-		 <form action="Acceuil.php" method="post">
-			  <input id="ButtonSeDeconnecter" type="submit" value="Se déconnecter">
-		 </form>
-		<label id="IdUser" for="IdUser">Bonjour [MAIL] </label>
-	</div>
-<<<<<<< HEAD
-   
-=======
-   
-   <div class="BlocAffGraphique">
-   <a href="GestionListeAdmin.php"><button id="BtnRetour" type="button">Retour</button></a>
+  
 
-    <table class="graph" cellspacing="6" cellpadding="0">
-      <thead>
-        <tr><th colspan="3">Graphique (Nombre de fournitures par matières)</th></tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td >Anglais</td><td class="bar"><div style="width: 13%"></div>1</td><td>12%</td>
-        </tr>
-        <tr>
-          <td>Espagnol</td><td class="bar"><div style="width: 50%"></div>5</td><td>58%</td>
-        </tr>
-        <tr>
-          <td>Allemand</td><td class="bar"><div style="width: 0%"></div>0</td><td>0%</td>
-        </tr>
-        <tr>
-          <td>Informatique</td><td class="bar"><div style="width: 0%"></div>0</td><td>0%</td>
-        </tr>
-        <tr>
-          <td>EPS</td><td class="bar"><div style="width: 0%"></div>0</td><td>0%</td>
-        </tr>
-        <tr>
-          <td>Philosophie</td><td class="bar"><div style="width: 13%"></div>1</td><td>13%</td>
-        </tr>
-      </tbody>
-    </table>
-	</div>
-
->>>>>>> origin/master
 </body>
 
 </html>
